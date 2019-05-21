66cc632262e836def7016e131317f49e93bf0fda
natahug <natahug@natahugs-MBP.DisStu.local>
2019-05-21 08:55:57 -0400

    description of your changes

---
+++ public/demo.js
@@ -0,0 +1,3 @@
+//alert("block!");
+//document.write("dude where's my page");
+//window.document.body.innerHTML = "???" //how to erase whole page
---
+++ public/notes.js
@@ -0,0 +1,17 @@
+//<script type="text/javascript">
+       // "use strict";
+
+        // function double(x){
+           //  return x * 2;
+        //  }
+        //  let thanker = function(name){
+        //      return "tak very much" + name;
+        //  };
+
+        //  let thankArrow = (name) => {
+        //     return "tak " + name;
+        //  };
+
+        // </script>
+
+        //console.log("hello world");
