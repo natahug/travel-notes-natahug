17d334bb9f67f95f2cbbfce0103e904d6f59dbc6
natahug <natahug@9470M-2055.DisStu.local>
2019-05-31 07:17:15 -0400

    run on port 8- in production

--- server/index.ts
+++ server/index.ts
@@ -36,5 +36,6 @@ app.get("/gallery", (req, res) => {
     {title: "Photo Gallery"});
 });
 
-app.listen(1234, () => console.log("Listening on 1234"))
+const PORT = process.env.NODE_ENV === "production" ? 80 : 1234;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
    .on("error", (e) => console.error(e));
