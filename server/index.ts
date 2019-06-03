1f022641e1b746d3f605a35e2aac6f427718613b
natahug <natahug@natahugs-MacBook-Pro.local>
2019-06-03 17:52:10 -0400

    added sql

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { DB, Rows } from "./db";
+
 import * as express from "express";
 import * as exphbs from "express-handlebars";
+import { async } from "q";
 
 let app = express();
 
@@ -12,13 +18,30 @@ app.engine("hbs", exphbs({
 
 app.use(express.static("dist/"));
 
-app.get("/", (req, res) => {
-    res.render("index",
-    {layout: "default-home.hbs",
-    title: "README"},
-    
-    );
+app.get("/", async(req, res) => {
+    let [rows] =  await DB.query<Rows>("SELECT * FROM posts ORDER BY time DESC");
+    res.render("index", {posts:rows, layout: "default-home.hbs",
+    title: "README"});
 });
+
+app.get("/todos.json", async (req, res)=> {
+    let results =  await DB.query<Rows>("SELECT * FROM todos");
+    let rows = results[0];
+    res.json(rows);
+});
+
+app.get("/todos", async (req, res)=> {
+    let [rows] =  await DB.query<Rows>("SELECT * FROM todos");
+    res.render("todos-demo", {todos:rows});
+
+});
+
+app.get("/todos/:id", async(req, res) => {
+    let [rows] = await DB.query<Rows>("SELECT * FROM todos WHERE id = :id", {id: req.params.id});
+    res.json(rows);
+});
+
+
 
 app.get("/about", (req, res) => {
     res.render("about",
@@ -36,6 +59,9 @@ app.get("/gallery", (req, res) => {
     {title: "Photo Gallery"});
 });
 
-const PORT = process.env.NODE_ENV === "production" ? 80 : 1234;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+export let main = async () => {
+//const PORT = process.env.NODE_ENV === "production" ? 80 : 1234;
+app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
    .on("error", (e) => console.error(e));
+};
+main();
